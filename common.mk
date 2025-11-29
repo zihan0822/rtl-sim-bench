2431977421df4f21570b70683a6fe79cfec40385
Zihan <zihanli0822@gmail.com>
2025-11-29 13:50:45 -0500

    feat: add repcut setup
    Signed-off-by: Zihan <zihanli0822@gmail.com>

--- common.mk
+++ common.mk
@@ -1,8 +1,12 @@
+ROOT_DIR := $(patsubst %/,%, $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 RISCV_DIR := $(shell pwd)/riscv
 RISCV_TARGET := riscv64-unknown-elf
 CXXFLAGS := -O3 -std=c++17
 INCLUDES := -I$(RISCV_DIR)/include
 LIBS := -L$(RISCV_DIR)/lib -Wl,-rpath,$(RISCV_DIR)/lib
+
+FIRRTL_SOURCE := $(ROOT_DIR)/chip-designs/rocket20.fir
+SBT_OPTS := "-Xmx4G -Xss4M"
 
 .PHONY: riscv clean
 
