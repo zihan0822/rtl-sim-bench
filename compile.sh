02ff0efdc5a69d989832a921877ab445b2643019
Zihan <zihanli0822@gmail.com>
2025-12-03 14:29:09 -0500

    feat: update compile.sh
    Signed-off-by: Zihan <zihanli0822@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 designs=("rocket20" "boom21")
-tools=("repcut-sim" "essent-sim")
+tools=("repcut-sim" "essent-sim" "patronus-sim" "verilator")
 
 for design in "${designs[@]}"; do
     for tool in "${tools[@]}"; do
-        make DESIGN=$design -C $tool all
+        nohup make DESIGN=$design -C $tool all &
     done
 done
 
